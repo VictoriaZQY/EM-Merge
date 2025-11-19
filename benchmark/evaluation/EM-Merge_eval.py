13b0c7c3029dc1699c96eb4b8e92bba84dd8b862
Victoria Zhu <qiyue.zhu@ucdconnect.ie>
2025-11-19 14:19:22 +0800

    Refactor EM-Merge evaluation script

--- benchmark/evaluation/EM-Merge_eval.py
+++ benchmark/evaluation/EM-Merge_eval.py
@@ -1,24 +1,9 @@
-"""
-This file is part of TA-Eval-Rep.
-Copyright (C) 2022 University of Luxembourg
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, version 3 of the License.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
 import sys
 import os
 
 sys.path.append('../')
 
-from logparser.LILAC import LogParser
+from logparser.EM-Merge import LogParser
 from evaluation.settings import benchmark_settings
 from evaluation.utils.common import common_args
 from evaluation.utils.evaluator_main import evaluator, prepare_results
@@ -51,7 +36,7 @@ if __name__ == "__main__":
     args = common_args()
     data_type = "full" if args.full_data else "2k"
     input_dir = f"../../{data_type}_dataset/"
-    output_dir = f"../../result/result_LILAC_{data_type}_{args.shot}_{args.example_size}_{args.model}"
+    output_dir = f"../../result/result_EMMerge_{data_type}_{args.shot}_{args.example_size}_{args.model}"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -94,4 +79,5 @@ if __name__ == "__main__":
             result_file=result_file,
         )  # it internally saves the results into a summary file
     metric_file = os.path.join(output_dir, result_file)
-    post_average(metric_file, f"LILAC_{data_type}_complex={args.complex}_frequent={args.frequent}_{args.shot}_{args.example_size}_{args.model}", args.complex, args.frequent)
+    post_average(metric_file, f"EMMerge_{data_type}_complex={args.complex}_frequent={args.frequent}_{args.shot}_{args.example_size}_{args.model}", args.complex, args.frequent)
+
